7bc9741d35e15ca00c507ac7c7d584c60f98ce6e
Ardan-213 <dutaardan635@gmail.com>
2025-04-16 10:18:57 +0700

    buat produk toko ku

---
+++ lib/models/produk_model.dart
@@ -0,0 +1,28 @@
+class ProdukModel {
+  final String id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+
+ProdukModel({
+  required this.id,
+  required this.title,
+  required this.price,
+  required this.description,
+  required this.category,
+  required this.image,
+  });
+
+  factory ProdukModel.fromJson(Map<String, dynamic> json) {
+    return ProdukModel(
+      id: json['id'],
+      title: json['title'],
+      price: (json['price'] as num). toDouble(),
+      description: json['description'],
+      category: json['category'],
+      image: json['image'],
+    );
+  }
+}
